3ac7e705f7505c4da637594e2b9414b89f1a210f
Akshay Jain <uniquejainakshay@gmail.com>
2016-03-15 01:26:28 +0530

    Q2. d Completed. Needs to be tested

---
+++ assignment2/q2/multiclass_nn.m
@@ -0,0 +1,121 @@
+function multiclass_nn()
+	j_theta_mean = 1000; 
+    error = 0.001; 
+    
+    % outputs
+    oi = zeros(784,1);
+    oh = zeros(100,1);
+    oj = zeros(4, 1) ; 
+    
+    %thetas
+    Tih = rand(785, 100, 1) .* 1e-5; 
+    Tho = rand(101, 4 , 1 ) .* 1e-5; 
+    
+    %expected outputs
+    tj = zeros(4, 1) ; 
+    
+    %gradients 
+    gih = zeros(785, 100);
+    gho = zeros(101, 4);
+    
+    % read training data
+    data = load('mnist_all.mat');
+    
+    x = containers.Map('KeyType', 'int32', 'ValueType', 'any') ; 
+    x(0) = double(data.train0)/255; 
+    x(1) = double(data.train1)/255; 
+    x(2) = double(data.train2)/255; 
+    x(3) = double(data.train3)/255; 
+    x(4) = double(data.train4)/255; 
+    x(5) = double(data.train5)/255; 
+    x(6) = double(data.train6)/255; 
+    x(7) = double(data.train7)/255; 
+    x(8) = double(data.train8)/255; 
+    x(9) = double(data.train9)/255; 
+    
+    t = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+    t(0) = [0 0 0 0]';
+    t(1) = [0 0 0 1]';
+    t(2) = [0 0 1 0]';
+    t(3) = [0 0 1 1]';
+    t(4) = [0 1 0 0]';
+    t(5) = [0 1 0 1]';
+    t(6) = [0 1 1 0]';
+    t(7) = [0 1 1 1]';
+    t(8) = [1 0 0 0]';
+    t(9) = [1 0 0 1]';
+    
+    m = containers.Map('KeyType', 'int32', 'ValueType', 'any');
+    for i = 0:9
+        [m(i),~ ]= size(x(i));
+    end
+    
+    it = 0 ; 
+    while j_theta_mean > error 
+        it = it + 1; 
+        j_theta_mean= 0 ; 
+        for e = 0:9 
+            %% iteration begin 
+                % choose the digit e 
+                xe = x(e);
+                oi = xe(randi([ 1 m(e)]), :)';
+                tj = t(e); 
+                
+                %forward pass 
+                oh = sigmoid([1;oi]' * Tih)';  % 100x1
+                oj = sigmoid([1;oh]' * Tho)';  % 4x1 
+                
+                %% backward pass 
+                %calculate gradients
+                delop = (tj - oj ) .* oj .* ( 1 - oj);     % 4x1 del output
+                gho =  -[1;oh] * delop';                      % 101x4 outer product
+                
+                delh =  ( oh ) .* ( 1 - oh ) .* (Tho(2:end,:) * delop);  % 100x 1 del hidden 
+                gih = -[1;oi] * delh'; % 785 x 100 outer product 
+                
+                %updated thetas 
+                eta = 1 / sqrt(it);
+                Tih = Tih - eta * gih ; 
+                Tho = Tho - eta * gho ; 
+                
+                %calculate j theta 
+                
+                jtheta = 0.5*(tj-oj)'*(tj-oj); 
+                j_theta_mean = j_theta_mean + jtheta ; 
+                
+            %% iteration end
+        end
+        j_theta_mean = j_theta_mean / 10 ; 
+    end
+    
+    %% training completed now testing
+    
+	x(0) = double(data.test0)/255; 
+    x(1) = double(data.test1)/255; 
+    x(2) = double(data.test2)/255; 
+    x(3) = double(data.test3)/255; 
+    x(4) = double(data.test4)/255; 
+    x(5) = double(data.test5)/255; 
+    x(6) = double(data.test6)/255; 
+    x(7) = double(data.test7)/255; 
+    x(8) = double(data.test8)/255; 
+    x(9) = double(data.test9)/255;
+    
+    for i = 0:9
+        [m(i),~ ]= size(x(i));
+    end
+    
+    for i = 0:9
+        correct = 0 ;
+        xi = x(i);
+        for j = 1:m(i) 
+            op = test_nn(xi(j,:), Tih, Tho);
+            if op == t(i) 
+                correct = correct+1 ; 
+            end
+        end
+        disp('Accuracy for' ) ; 
+        disp(i);    
+        disp(100 * correct / m(i));
+    end
+end
--- assignment2/q2/test_nn.m
+++ assignment2/q2/test_nn.m
@@ -1,9 +1,9 @@
 function [val] = test_nn(x, Tih, Tho ) 
     %x : 1x784  
     % Tih 785 x 100 
-    % Tho 101 x 1 
+    % Tho 101 x 4 
     oh = [1 x] * Tih ; % 1 x 100 
     op = [1 oh ] * Tho ; % 1 x 1 
-    val = op >= 0.5; 
+    val = op' >= 0.5; 
 end
     
